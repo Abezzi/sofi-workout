6ad87445941e90511e29bed1771345ae794e678b
wizwiz <abezzi@outlook.cl>
2025-09-05 11:11:31 -0400

    style(category): category list style with safe area view

--- components/category/category-list.tsx
+++ components/category/category-list.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { FlatList, Pressable, StyleSheet, View } from 'react-native';
 import { Text } from '@/components/ui/text';
 import { Category } from '@/db/schema';
+import { Separator } from '../ui/separator';
+import { SafeAreaView } from 'react-native-safe-area-context';
+// import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type CategoryListProps = {
   categories: Category[];
@@ -9,40 +12,59 @@ type CategoryListProps = {
 };
 
 export function CategoryList({ categories, onCategoryPress }: CategoryListProps) {
+  // const insets = useSafeAreaInsets();
+
   const renderItem = ({ item }: { item: Category }) => (
-    <Pressable onPress={() => onCategoryPress?.(item)}>
-      <Text>
-        <Text
-          style={{
-            color: item.color,
-            fontSize: 48,
-            fontWeight: '500',
-          }}>
-          ■
+    <Pressable onPress={() => onCategoryPress?.(item)} style={styles.itemContainer}>
+      <View style={styles.textContainer}>
+        <Text>
+          <Text
+            style={{
+              color: item.color,
+              fontSize: 24,
+              fontWeight: '500',
+            }}>
+            ■
+          </Text>
+          <Text
+            style={{
+              fontSize: 36,
+              fontWeight: '500',
+            }}>
+            {item.name}
+          </Text>
         </Text>
-        <Text
-          style={{
-            fontSize: 48,
-            fontWeight: '500',
-          }}>
-          {item.name}
-        </Text>
-      </Text>
+      </View>
     </Pressable>
   );
 
   return (
-    <FlatList
-      data={categories}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id.toString()}
-      contentContainerStyle={styles.listContainer}
-    />
+    <SafeAreaView>
+      <FlatList
+        data={categories}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+        ItemSeparatorComponent={Separator}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  itemContainer: {
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
   listContainer: {
     padding: 16,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
