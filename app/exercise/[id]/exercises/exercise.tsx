ecc0cb2d1c81a5a7edeb822939a8dcc221354b0a
wizwiz <abezzi@outlook.cl>
2025-09-06 12:13:32 -0400

    feat(exercise): loading indicator and header title style
    plus a bit of code style for better readability

--- app/exercise/[id]/exercises/exercise.tsx
+++ app/exercise/[id]/exercises/exercise.tsx
@@ -1,4 +1,5 @@
-import { useLocalSearchParams } from 'expo-router';
+import { ActivityIndicator, View } from 'react-native';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { ExerciseList } from '@/components/exercise/exercise-list';
@@ -6,34 +7,49 @@ import { Text } from '@/components/ui/text';
 import { getCategoryById } from '@/db/queries/category.queries';
 import { getAllExercisesByCategoryId } from '@/db/queries/exercise.queries';
 import { Category, Exercise } from '@/db/schema';
+import { useColorScheme } from 'nativewind';
 
 export default function Screen() {
   const { id } = useLocalSearchParams() as { id: string };
   const [exercisesByCategory, setExercisesByCategory] = useState<Exercise[]>([]);
+  const [loadingExercises, setLoadingExercises] = useState<boolean>(false);
   const [category, setCategory] = useState<Category>({
     id: 0,
     name: '',
     color: '',
   });
+  const { colorScheme } = useColorScheme();
+  const navigation = useNavigation();
+  const router = useRouter();
   let exercisesData: Exercise[];
   let categoryData: Category;
-  const router = useRouter();
+
+  const loadExercisesByCategory = async () => {
+    setLoadingExercises(true);
+    const data = await getAllExercisesByCategoryId(parseInt(id));
+    const dataCategory = await getCategoryById(parseInt(id));
+
+    if (data !== undefined) {
+      exercisesData = data;
+    }
+
+    if (dataCategory !== undefined) {
+      categoryData = dataCategory;
+    }
+
+    setCategory(categoryData);
+    setExercisesByCategory(exercisesData);
+    setLoadingExercises(false);
+  };
+
+  function changeNavigationTitle() {
+    navigation.setOptions({ title: `Exercises` });
+  }
 
   useEffect(() => {
-    const loadExercisesByCategory = async () => {
-      const data = await getAllExercisesByCategoryId(parseInt(id));
-      const dataCategory = await getCategoryById(parseInt(id));
-      if (data !== undefined) {
-        exercisesData = data;
-      }
-      if (dataCategory !== undefined) {
-        categoryData = dataCategory;
-      }
-      setCategory(categoryData);
-      setExercisesByCategory(exercisesData);
-    };
     loadExercisesByCategory();
-  }, []);
+    changeNavigationTitle();
+  }, [navigation]);
 
   const handleExercisePress = (exercise: Exercise) => {
     console.log('pressed exercise list');
@@ -42,11 +58,18 @@ export default function Screen() {
 
   return (
     <>
-      <ExerciseList
-        exercises={exercisesByCategory}
-        onExercisePress={handleExercisePress}
-        category={category}
-      />
+      {loadingExercises ? (
+        <View className="items-center justify-center">
+          <ActivityIndicator size="large" color={colorScheme === 'dark' ? '#FFF' : '#000'} />
+          <Text>Loading Exercises...</Text>
+        </View>
+      ) : (
+        <ExerciseList
+          exercises={exercisesByCategory}
+          onExercisePress={handleExercisePress}
+          category={category}
+        />
+      )}
     </>
   );
 }
