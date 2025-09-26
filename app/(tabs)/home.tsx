4d262431ff282db4653b644c615ff6ca1ccc681a
wizwiz <abezzi@outlook.cl>
2025-09-26 20:35:39 -0300

    feat(routine): select template to search a routine and load it

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,13 +1,43 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Text } from '@/components/ui/text';
-import { useEffect } from 'react';
-import { View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { View, Platform } from 'react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { router } from 'expo-router';
+import { Separator } from '@/components/ui/separator';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { TriggerRef } from '@rn-primitives/select';
+import { Routine } from '@/db/schema';
+import { getAllRoutines } from '@/db/queries/routine.queries';
+import { Label } from '@/components/ui/label';
+
+type Option = {
+  value: string;
+  label: string;
+};
 
 export default function Screen() {
   const insets = useSafeAreaInsets();
+  const [selectedRoutine, setSelectedRoutine] = useState<Option | undefined>(undefined);
+  const [loadingRoutines, setLoadingRoutines] = useState<boolean>(false);
+  const [routineData, setRoutineData] = useState<Routine[]>([]);
+  const ref = useRef<TriggerRef>(null);
+  const contentInsets = {
+    top: insets.top,
+    bottom: Platform.select({
+      ios: insets.bottom,
+      android: insets.bottom + 24,
+    }),
+  };
 
   function handleNewRoutine() {
     router.push({ pathname: '/routine/new-routine' });
@@ -17,12 +47,26 @@ export default function Screen() {
     router.push({ pathname: '/routine/copy-routine' });
   }
 
-  useEffect(() => { }, []);
+  const loadAllRoutines = async () => {
+    setLoadingRoutines(true);
+    const data = await getAllRoutines();
+    if (data !== undefined) setRoutineData(data);
+    setLoadingRoutines(false);
+  };
+
+  useEffect(() => {
+    loadAllRoutines();
+  }, []);
+
+  const handleStart = () => {
+    console.log('starting...');
+  };
 
   return (
     <SafeAreaView className="flex-1" style={{ paddingBottom: insets.bottom }}>
       <Card className="border-border/0 shadow-none sm:border-border sm:shadow-sm sm:shadow-black/5">
         <CardHeader>
+          {/*Buttons Top*/}
           <View className="flex-row items-center justify-center">
             <Button onPress={handleNewRoutine}>
               <Text>New Routine</Text>
@@ -35,6 +79,37 @@ export default function Screen() {
             </Button>
           </View>
         </CardHeader>
+
+        <CardContent>
+          {/*Routine Select*/}
+          <View>
+            <Label htmlFor="routine">Routine to load:</Label>
+            <Select
+              id="routine"
+              disabled={loadingRoutines}
+              value={selectedRoutine}
+              onValueChange={(option) => setSelectedRoutine(option)}>
+              <SelectTrigger ref={ref}>
+                <SelectValue placeholder="Select a Routine..." className="text-lg" />
+              </SelectTrigger>
+              <SelectContent insets={contentInsets} className="max-h-96 w-auto rounded-md">
+                <SelectGroup>
+                  <SelectLabel className="text-lg">routines</SelectLabel>
+                  {routineData.map((routine) => (
+                    <SelectItem key={routine.id} label={routine.name} value={routine.id.toString()}>
+                      {routine.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </View>
+        </CardContent>
+        <CardFooter className="flex-col gap-3 pb-0">
+          <Button onPress={handleStart}>
+            <Text>Start</Text>
+          </Button>
+        </CardFooter>
       </Card>
     </SafeAreaView>
   );
