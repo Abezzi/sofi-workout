31ab3c93a2b246797b01dc52619fb1628ad444d4
wizwiz <abezzi@outlook.cl>
2025-10-09 16:47:40 -0300

    db(rest-timer): table to handle the rest timers

---
+++ db/schema/rest_timer.ts
@@ -0,0 +1,21 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { routine_exercise } from './routine_exercise';
+import { routine } from './routine';
+import { exercise_set } from './exercise_set';
+
+const restTimerTypes = ['set', 'exercise'] as const;
+export type RestTimerType = (typeof restTimerTypes)[number];
+
+export const rest_timer = sqliteTable('rest_timer', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  routineId: integer('routine_id')
+    .notNull()
+    .references(() => routine_exercise.id),
+  routineExerciseId: integer('routine_exercise_id').references(() => routine.id),
+  exerciseSetId: integer('exercise_set_id').references(() => exercise_set.id),
+  restTime: integer('rest_time').notNull(),
+  type: text('type', { enum: restTimerTypes }).notNull(),
+});
+
+export type RestTimer = typeof rest_timer.$inferSelect;
+export default { rest_timer };
