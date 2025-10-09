08800aaea468046a4efbde422a8f12d5d66862c9
wizwiz <abezzi@outlook.cl>
2025-10-09 17:41:37 -0300

    feat(drizzle): migration with the new table rest-timer

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -5,6 +5,8 @@ import m0000 from './0000_windy_lilandra.sql';
 import m0001 from './0001_ancient_sentry.sql';
 import m0002 from './0002_wandering_lady_mastermind.sql';
 import m0003 from './0003_cute_madrox.sql';
+import m0004 from './0004_faulty_expediter.sql';
+import m0005 from './0005_mature_vulcan.sql';
 
   export default {
     journal,
@@ -12,7 +14,9 @@ import m0003 from './0003_cute_madrox.sql';
       m0000,
 m0001,
 m0002,
-m0003
+m0003,
+m0004,
+m0005
     }
   }
   
