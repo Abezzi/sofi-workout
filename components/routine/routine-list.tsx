f21c91625402b94d65676531a9bb1b1dca141303
wizwiz <abezzi@outlook.cl>
2025-10-20 10:23:01 -0300

    style(code): order of the code and delete handler

--- components/routine/routine-list.tsx
+++ components/routine/routine-list.tsx
@@ -2,6 +2,7 @@ import { FlatList, Pressable, View } from 'react-native';
 import { Text } from '../ui/text';
 import { Card } from '@/components/ui/card';
 import {
+  deleteRoutineById,
   getRoutinesWithExerciseAndRest,
   RoutineWithExerciseAndRest,
 } from '@/db/queries/routine.queries';
@@ -34,134 +35,10 @@ interface RoutineItemProps {
   setActiveItem: (id: number | null) => void;
 }
 
-const RoutineItem = memo(({ name, exercises, id, isActive, setActiveItem }: RoutineItemProps) => {
-  const longPressRef = useRef(false);
-  const [showIcons, setShowIcons] = useState(false);
-  const opacity = useSharedValue(0);
-  const translateX = useSharedValue(-30);
-  const width = useSharedValue(0);
-
-  // filter exercises to get unique categories
-  const uniqueCategories = exercises.filter(
-    (exercise, index, self) =>
-      exercise.category && index === self.findIndex((e) => e.category?.id === exercise.category?.id)
-  );
-
-  useEffect(() => {
-    opacity.value = withTiming(isActive ? 1 : 0, { duration: 200 });
-    translateX.value = withTiming(isActive ? 0 : -30, { duration: 200 });
-    width.value = withTiming(isActive ? 24 : 0, { duration: 200 });
-  }, [isActive, opacity, translateX, width]);
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: opacity.value,
-    transform: [{ translateX: translateX.value }],
-    width: width.value,
-    pointerEvents: opacity.value > 0 ? 'auto' : 'none',
-  }));
-
-  return (
-    <View className="flex flex-row gap-2 rounded-lg border border-primary bg-gray-100 p-3 dark:bg-primary-foreground">
-      {/* view at the left side */}
-      <Animated.View className="flex flex-col justify-between" style={animatedStyle}>
-        <Pressable
-          onPress={() => {
-            handleOnEdit(id);
-            setActiveItem(null);
-          }}>
-          <Icon as={Pencil} className="size-6" />
-        </Pressable>
-        <Pressable
-          onPress={() => {
-            handleOnDelete(id);
-            setActiveItem(null);
-          }}>
-          <Icon as={X} className="size-6" />
-        </Pressable>
-        <Pressable
-          onPress={() => {
-            handleOnView(id);
-            setActiveItem(null);
-          }}>
-          <Icon as={Eye} className="size-6" />
-        </Pressable>
-      </Animated.View>
-      {/* view at the right side */}
-      <View className="flex-1">
-        <Pressable
-          onPress={() => {
-            if (!longPressRef.current) {
-              handleOnPress(id);
-              setShowIcons(false);
-            }
-            longPressRef.current = false;
-          }}
-          onLongPress={() => {
-            longPressRef.current = true;
-            // keep the pressed routine item id saved to hide the options of others routine items
-            setActiveItem(isActive ? null : id);
-            // little vibration when long press
-            Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
-            setShowIcons((prev) => !prev);
-          }}
-          className="w-48 items-center">
-          <Text
-            className="line-clamp-2 break-words text-center align-middle text-base font-medium"
-            numberOfLines={2}
-            ellipsizeMode="tail"
-            style={{ minHeight: 55 }}>
-            {name}
-          </Text>
-          <View className="flex flex-row">
-            {uniqueCategories.length > 0 ? (
-              uniqueCategories.map((exercise) =>
-                exercise.category ? (
-                  <Text key={exercise.category.id} style={{ color: `${exercise.category.color}` }}>
-                    ■
-                  </Text>
-                ) : null
-              )
-            ) : (
-              <Text>No categories</Text>
-            )}
-          </View>
-        </Pressable>
-      </View>
-    </View>
-  );
-});
-
-const handleOnPress = (id: number) => {
-  router.push({
-    pathname: '/(tabs)/workout',
-    params: { selectedRoutine: id },
-  });
-};
-
-const handleOnEdit = (id: number) => {
-  router.push({
-    pathname: '/(tabs)/workout',
-    params: { selectedRoutine: id },
-  });
-};
-
-const handleOnDelete = (id: number) => {
-  router.push({
-    pathname: '/(tabs)/workout',
-    params: { selectedRoutine: id },
-  });
-};
-
-const handleOnView = (id: number) => {
-  router.push({
-    pathname: '/(tabs)/workout',
-    params: { selectedRoutine: id },
-  });
-};
-
 export default function RoutineList() {
   const [routines, setRoutines] = useState<RoutineWithExerciseAndRest[]>();
   const [activeItemId, setActiveItemId] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getRoutines = async () => {
     try {
@@ -192,11 +69,137 @@ export default function RoutineList() {
     return (
       <View className="items-center">
         <Text className="font-semibold">Your routine list is empty</Text>
-        <Text className="text-sm text-muted-foreground">add some by pressing 'New Routine'</Text>
+        <Text className="text-sm text-muted-foreground">
+          add routines by pressing 'New Routine'
+        </Text>
         <Icon as={Ghost} className="size-8" />
       </View>
     );
   };
+
+  const RoutineItem = memo(({ name, exercises, id, isActive, setActiveItem }: RoutineItemProps) => {
+    const longPressRef = useRef(false);
+    const opacity = useSharedValue(0);
+    const translateX = useSharedValue(-30);
+    const width = useSharedValue(0);
+
+    // filter exercises to get unique categories
+    const uniqueCategories = exercises.filter(
+      (exercise, index, self) =>
+        exercise.category &&
+        index === self.findIndex((e) => e.category?.id === exercise.category?.id)
+    );
+
+    useEffect(() => {
+      opacity.value = withTiming(isActive ? 1 : 0, { duration: 200 });
+      translateX.value = withTiming(isActive ? 0 : -30, { duration: 200 });
+      width.value = withTiming(isActive ? 24 : 0, { duration: 200 });
+    }, [isActive, opacity, translateX, width]);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+      opacity: opacity.value,
+      transform: [{ translateX: translateX.value }],
+      width: width.value,
+      pointerEvents: opacity.value > 0 ? 'auto' : 'none',
+    }));
+
+    const handleOnPress = (id: number) => {
+      router.push({
+        pathname: '/(tabs)/workout',
+        params: { selectedRoutine: id },
+      });
+    };
+
+    const handleOnEdit = (id: number) => {
+      router.push({
+        pathname: '/(tabs)/workout',
+        params: { selectedRoutine: id },
+      });
+    };
+
+    const handleOnDelete = async (id: number) => {
+      setLoading(true);
+      await deleteRoutineById(id);
+      setLoading(false);
+    };
+
+    const handleOnView = (id: number) => {
+      router.push({
+        pathname: '/(tabs)/workout',
+        params: { selectedRoutine: id },
+      });
+    };
+
+    return (
+      <View className="flex flex-row gap-2 rounded-lg border border-primary bg-gray-100 p-3 dark:bg-primary-foreground">
+        {/* view at the left side */}
+        <Animated.View className="flex flex-col justify-between" style={animatedStyle}>
+          <Pressable
+            onPress={() => {
+              handleOnEdit(id);
+              setActiveItem(null);
+            }}>
+            <Icon as={Pencil} className="size-6" />
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              handleOnView(id);
+              setActiveItem(null);
+            }}>
+            <Icon as={Eye} className="size-6" />
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              handleOnDelete(id);
+              setActiveItem(null);
+            }}>
+            <Icon as={X} className="size-6" />
+          </Pressable>
+        </Animated.View>
+        {/* view at the right side */}
+        <View className="flex-1">
+          <Pressable
+            onPress={() => {
+              if (!longPressRef.current) {
+                handleOnPress(id);
+              }
+              longPressRef.current = false;
+            }}
+            onLongPress={() => {
+              longPressRef.current = true;
+              // keep the pressed routine item id saved to hide the options of others routine items
+              setActiveItem(isActive ? null : id);
+              // little vibration when long press
+              Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+            }}
+            className="w-48 items-center">
+            <Text
+              className="line-clamp-2 break-words text-center align-middle text-base font-medium"
+              numberOfLines={2}
+              ellipsizeMode="tail"
+              style={{ minHeight: 55 }}>
+              {name}
+            </Text>
+            <View className="flex flex-row">
+              {uniqueCategories.length > 0 ? (
+                uniqueCategories.map((exercise) =>
+                  exercise.category ? (
+                    <Text
+                      key={exercise.category.id}
+                      style={{ color: `${exercise.category.color}` }}>
+                      ■
+                    </Text>
+                  ) : null
+                )
+              ) : (
+                <Text>No categories</Text>
+              )}
+            </View>
+          </Pressable>
+        </View>
+      </View>
+    );
+  });
 
   return (
     <Card className="m-0 p-0">
@@ -219,6 +222,9 @@ export default function RoutineList() {
           getItemLayout={getItemLayout}
           ItemSeparatorComponent={() => <View className="w-1" />}
           ListEmptyComponent={emptyListComponent}
+          refreshing={loading}
+          // centers the empty list component
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}
         />
       </>
     </Card>
