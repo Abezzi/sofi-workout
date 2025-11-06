aec8aa4625af29c5707bfc1f5e7e0cd28f0433ef
wizwiz <abezzi@outlook.cl>
2025-11-06 16:27:17 -0300

    feat(full-screen-loader): optional message prop

--- components/base/full-screen-loader.tsx
+++ components/base/full-screen-loader.tsx
@@ -1,13 +1,20 @@
 import { ActivityIndicator, Modal, View } from 'react-native';
 import { Text } from '@/components/ui/text';
 
-export default function FullScreenLoader({ visible }: { visible: boolean }) {
+export type FullScreenLoaderType = {
+  visible: boolean;
+  message?: string;
+};
+
+export default function FullScreenLoader({ visible, message }: FullScreenLoaderType) {
   return (
     <Modal transparent animationType="fade" visible={visible}>
       <View className="flex-1 items-center justify-center bg-black/50">
         <View className="rounded-2xl bg-white p-8 shadow-lg">
           <ActivityIndicator size="large" color="#3b82f6" />
-          <Text className="mt-4 text-lg font-semibold text-gray-700">Saving...</Text>
+          <Text className="mt-4 text-lg font-semibold text-gray-700">
+            {message ? message : 'Loading...'}
+          </Text>
         </View>
       </View>
     </Modal>
